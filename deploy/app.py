f4038894182c63930e09e96eb3e3546b4bce3d9e
Philip1435 <6000189@kvis.ac.th>
2024-03-08 03:46:14 +0700

    fix model path

--- deploy/app.py
+++ deploy/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from model import RacistContentClassifier
 
-model_path = './rf_model.joblib'
+model_path = 'rf_model.joblib'
 classifier = RacistContentClassifier(model_path)
 
 st.title('Racist Content Classifier')
